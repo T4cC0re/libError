df856d5190c01d199644d3563e2bfc12ce37c363
Hendrik Jonas Meyer <h.meyer@bigpoint.net>
2017-09-13 11:57:02 +0200

    Initial commit

---
+++ src/libError.ts
@@ -0,0 +1,84 @@
+/**
+ * Copyright 2017 Hendrik 'T4cC0re' Meyer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+const stackman = require('stackman')();
+
+type libErrorFunc = (err: Error, message?: string, kill?: boolean) => void;
+
+declare module NodeJS {
+    interface Global {
+        logErr: libErrorFunc
+        libError: libErrorFunc
+    }
+}
+
+declare const libError: libErrorFunc;
+declare const logErr: libErrorFunc;
+
+global.libError = (err: Error, message?: string, kill?: boolean) => {
+    stackman.callsites(err, function (_err: Error, callsites: any[]) {
+        if (_err) {
+            throw _err;
+        }
+
+        const buffer: string[] = [];
+        for (let i = 0; i < callsites.length; i++) {
+            const callsite = callsites[i];
+            let flags = `${callsite.isApp() ? 'A' : '.'}${callsite.isToplevel() ? 'G' : '.'}${callsite.isModule() ? 'M' : '.'}${callsite.isNode() ? 'C' : '.'}${callsite.isNative() ? 'N' : '.'}${callsite.isEval() ? 'E' : '.'}${callsite.isConstructor() ? 'S' : '.'}`;
+
+            let prefix = `${flags}\t[${i}] ->`;
+            if (i == 0) {
+                prefix = `[${err.name}] ${err.message}\noccurred in\n${flags}\t[${i}] ->`
+                if (message) {
+                    prefix = message + ' ' + prefix;
+                }
+            }
+
+            let line: string = '';
+            if (callsite.isNode()) {
+                line = '<node core>/';
+            }
+            line += `${callsite.getRelativeFileName()}:${callsite.getLineNumber()} (`;
+            if (callsite.getTypeName()) {
+                line += `${callsite.getTypeName()}::`;
+            }
+            line += `${callsite.getFunctionNameSanitized()})`;
+
+            buffer.push(`${prefix} ${line}`);
+        }
+        console.error(
+            `ERROR --- ${(new Date()).toUTCString()}\n${
+                buffer.join('\n')
+                }\n}----------------------------------------------`
+        );
+        if (kill) {
+            process.exit(1);
+        }
+    });
+};
+
+global.logErr = global.libError;
+
+process.on('unhandledRejection', (reason: Error) => {
+    libError(reason, 'unhandledRejection:', true);
+}).on('uncaughtException', (err: Error) => {
+    libError(err, 'uncaughtException:', true);
+}).on('warning', (warning: Error) => {
+    console.warn(warning.name);    // Print the warning name
+    console.warn(warning.message); // Print the warning message
+    console.warn(warning.stack);   // Print the stack trace
+});
